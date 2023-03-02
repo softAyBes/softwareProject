b79f01c8b621b4263cc1acf669164d9c929d2704
aya-baara <124375259+aya-baara@users.noreply.github.com>
2023-03-02 23:47:39 +0300

    Update customer.java
    cutomer extends person

--- src/main/java/carpetcleaning/customer.java
+++ src/main/java/carpetcleaning/customer.java
@@ -1,17 +1,22 @@
 package carpetcleaning;
 
-public class customer {
+public class customer extends person{
 	private String id;
 	private String name;
 	private int phone;
 	private String address;
+
+	public customer(){
+		super();
+		this.type="client";
+	}
 	
 	//private Main main=new Main();
 	
 	public String getId() {
 		return id;
 	}
-	public void setId(String id2) {
+	public void setId(String id2) { 
 		this.id = id2;
 	}
 	
@@ -37,29 +42,34 @@ public class customer {
 	}
 	public static int getIndex(String id) {
 		int index=-1;
+		
 		for(int i=0;i<Main.customers.size();i++) {
+			
 			if(Main.customers.get(i).id.equalsIgnoreCase(id)) {
+				
 				return i;
 			}
 		}
 		
 		return index;
-		
 	}
-	public boolean checkDublicate(String id2) {
+	public boolean isExist(String id2) {
 		// TODO Auto-generated method stub
 		// create common class contains all arrays list (customer, product ......)
-		if(getIndex(id2)==-1) return false;
+		if(getIndex(id2)==-1) {
+			
+			return false;
+		}
 		
 		return true;
-		
 	}
+	
+	
 	
 	
 	public static void createCustomer(customer cust) {
 		// TODO Auto-generated method stub
-		Main.customers.add(cust);
-		
+		Main.customers.add(cust);	
 	}
 	
 
