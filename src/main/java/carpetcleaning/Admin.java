5365a728b6516ee9d633c826ab93dbf1875adfb5
aya-baara <124375259+aya-baara@users.noreply.github.com>
2023-03-05 06:34:22 +0300

    Update Admin.java

--- src/main/java/carpetcleaning/Admin.java
+++ src/main/java/carpetcleaning/Admin.java
@@ -1,37 +1,47 @@
 package carpetcleaning;
 
-public class Admin {
-	private String name;
-	private String pass;
-	private boolean status;
+import java.util.ArrayList;
+
+public class worker extends person{
+	private String phone;
+	private String address;
+	private String salary;
+	private  ArrayList<product> productsForWorker=new ArrayList<product>();
+	public String getPhone() {
+		return phone;
+	}
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getSalary() {
+		return salary;
+	}
+	public void setSalary(String salary) {
+		this.salary = salary;
+	}
+	public ArrayList<product> getProductsForWorker() {
+		return productsForWorker;
+	}
+	public void setProductsForWorker(ArrayList<product> productsForWorker) {
+		this.productsForWorker = productsForWorker;
+	}
+	public static String updateStatusCustomer(String code,String id,String newStatus) {
+		customer cust=new customer();
+		int CustomerIndex=customer.getIndex(id);
+	    cust=Main.customers.get(CustomerIndex);
+	    int productIndex=cust.getIndexforProduct(code);
+	    Main.customers.get(CustomerIndex).getProductsForCusomer().get(productIndex).setStatus(newStatus);
+	    return Main.customers.get(CustomerIndex).getProductsForCusomer().get(productIndex).getStatus();
+	}
 	
-	public Admin() {
-		this.status=false;
-		this.pass="IamAdmin";
-		this.name="admin";
-	}
+	
+	
 
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public String getPass() {
-		return pass;
-	}
-
-	public void setPass(String pass) {
-		this.pass = pass;
-	}
-
-	public boolean isStatus() {
-		return status;
-	}
-
-	public void setStatus(boolean status) {
-		this.status = status;
-	}
+	
 }
