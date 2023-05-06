05ab82211894ebd0b58798df9cf62c773ea332fd
Beesan <Beesan@DESKTOP-L0JGFCJ>
2023-05-06 13:41:46 +0300

    test

--- src/main/java/carpetcleaning/Main.java
+++ src/main/java/carpetcleaning/Main.java
@@ -5,7 +5,7 @@ import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-public class Main {
+public class Main{
 
 	protected static final List<Customer> customers = new ArrayList<>();
 	protected static final List<Person> persons = new ArrayList<>();
